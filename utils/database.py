b301b2975a46a10c1444b6a910b1633d833fee02
partiallyZen <42852344+partiallyZen@users.noreply.github.com>
2023-02-28 12:58:37 -0600

    added database.py

---
+++ utils/database.py
@@ -0,0 +1,27 @@
+"""
+Concerned with storing and retrieving books from a list.
+"""
+
+books = []
+
+def add_book(name, author):
+    books.append({'name': name, 'author': author, 'read': False})
+
+
+def get_all_books():
+    return books
+
+
+def mark_book_as_read(name):
+    for book in books:
+        if book['name'] == name:
+            book['read'] == True
+
+def delete_book(name):
+    global books
+    books = [book for book in books if book['name'] != name] #add each book to new list if book['name'] != name
+
+# def delete_book(name):
+#    for book in books:
+#        if book['name'] == name:
+#            books.remove(book)
